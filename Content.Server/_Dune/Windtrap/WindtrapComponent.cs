cf32d9dfda9c0931103715d64578570c4ea06b37
BeBright <98597725+be1bright@users.noreply.github.com>
2025-10-08 16:43:52 +0300

    windtrap

---
+++ Content.Server/_Dune/Windtrap/WindtrapComponent.cs
@@ -0,0 +1,39 @@
+using Content.Shared.Chemistry.Components;
+using Content.Shared.FixedPoint;
+using Robust.Shared.GameStates;
+
+namespace Content.Server._Dune.Windtrap;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class WindtrapComponent : Component
+{
+    /// <summary>
+    /// The solution inside of windtrap.
+    /// </summary>
+    [ViewVariables]
+    public Entity<SolutionComponent>? Solution;
+
+    /// <summary>
+    /// What solution should windtrap contain (not reagent)?
+    /// </summary>
+    [DataField]
+    public string WindtrapSolutionName = "windtrap";
+
+    /// <summary>
+    /// Water gain per sec.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public FixedPoint2 WaterGainPerSecond = 1;
+
+    /// <summary>
+    /// The next time when the water will be gained.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public TimeSpan NextGainTime = TimeSpan.Zero;
+
+    /// <summary>
+    /// How often should windtrap get water?
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public TimeSpan GainInterval = TimeSpan.FromSeconds(1);
+}
---
+++ Content.Server/_Dune/Windtrap/WindtrapSystem.cs
@@ -0,0 +1,71 @@
+using Content.Server.Atmos.Components;
+using Content.Shared.Chemistry.Components.SolutionManager;
+using Content.Shared.Chemistry.EntitySystems;
+using Content.Shared.Coordinates;
+using Content.Shared.Light.Components;
+using Content.Shared.Light.EntitySystems;
+using Content.Shared.Maps;
+using Content.Shared.Weather;
+using Robust.Shared.Map;
+using Robust.Shared.Map.Components;
+using Robust.Shared.Timing;
+
+namespace Content.Server._Dune.Windtrap;
+
+public sealed class WindtrapSystem : EntitySystem
+{
+    [Dependency] private readonly IGameTiming _gameTiming = default!;
+    [Dependency] private readonly SharedSolutionContainerSystem _solutionContainerSystem = default!;
+    [Dependency] private readonly SharedTransformSystem _transform = default!;
+    [Dependency] private readonly SharedRoofSystem _roof = default!;
+    [Dependency] private readonly SharedMapSystem _mapSystem = default!;
+    [Dependency] private readonly TurfSystem _turf = default!;
+    [Dependency] private readonly ITileDefinitionManager _tileDefManager = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<WindtrapComponent, ComponentStartup>(OnComponentStartup);
+    }
+
+    private void OnComponentStartup(EntityUid uid, WindtrapComponent component, ComponentStartup args) =>
+        component.NextGainTime += _gameTiming.CurTime + component.GainInterval;
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var query = EntityQueryEnumerator<WindtrapComponent, SolutionContainerManagerComponent>();
+        while (query.MoveNext(out var uid, out var windtrap, out var sol))
+        {
+            var xform = Transform(uid);
+            var gridUid = xform.GridUid;
+
+            if (gridUid == null ||
+                !TryComp<MapGridComponent>(gridUid, out var grid))
+                continue;
+
+            var tilePos = _mapSystem.TileIndicesFor(gridUid.Value, grid, xform.Coordinates);
+            var tileRef = _turf.GetTileRef(xform.Coordinates);
+
+            if (TryComp<RoofComponent>(gridUid.Value, out var roofComp) &&
+                _roof.IsRooved((gridUid.Value, grid, roofComp), tilePos))
+                continue;
+
+            if (tileRef == null)
+                continue;
+
+            var tileDef = _turf.GetContentTileDefinition(tileRef.Value);
+            if (!tileDef.Weather)
+                continue;
+
+            if (_gameTiming.CurTime < windtrap.NextGainTime ||
+                !_solutionContainerSystem.ResolveSolution((uid, sol), windtrap.WindtrapSolutionName, ref windtrap.Solution, out _))
+                continue;
+
+            windtrap.NextGainTime += windtrap.GainInterval;
+            _solutionContainerSystem.TryAddReagent(windtrap.Solution.Value, "Water", windtrap.WaterGainPerSecond);
+        }
+    }
+}
